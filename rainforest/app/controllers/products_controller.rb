1a794a6feaa4e4a186f3bcc112d35436afaab551
Lia Forgione <liaforg@gmail.com>
2017-06-13 16:28:09 -0400

    new product form

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -9,9 +9,20 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+  end
+
+  def product_params
+    {name: params[:product][:name], description: params[:product][:description], price_in_cents: params[:product][:price_in_cents]}
   end
 
   def create
+    @product = Product.create(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
   def edit
