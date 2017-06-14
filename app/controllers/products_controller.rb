48cd2b1869b36a4e3ca376057f9cb7d38d7c428c
DocPrior <Sara Prior>
2017-06-14 14:14:56 -0400

    delete product define nav flash, and dollar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.create(product_params)
     if @product.save
+      flash[:notice] = "Product saved!"
       redirect_to products_path
     else
       render :new
@@ -32,6 +33,7 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
+      flash[:notice] = "Product udpated!"
       redirect_to @product
     else
       redirect_back_or_to @product
@@ -41,6 +43,7 @@ class ProductsController < ApplicationController
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
+    flash[:alert] = "Product deleted!"
     redirect_to "/products"
   end
 
