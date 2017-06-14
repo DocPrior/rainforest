a9dcb7affe2aaebbcc70d1cd5aa631e7cde0ca1b
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-14 16:21:10 -0400

    Add Edit Form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,16 @@
 class ReviewsController < ApplicationController
 
   def create
+    @review = Review.create(review_params)
+    if @review.save
+      flash[:notice] = "Review saved!"
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
 
   end
 
@@ -11,5 +21,9 @@ class ReviewsController < ApplicationController
   def destroy
 
   end
-  
+
+  def review_params
+    {comment: params[:review][:comment], rating: [:review][:rating], product_id: params[:product_id]}
+  end
+
 end
